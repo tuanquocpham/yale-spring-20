9745e6e33a944c2712420d163d02ed7eef80afcf
tuan <tuan.pham@yale.edu>
2020-04-21 00:56:26 -0400

    update

---
+++ assets/site-script.js
@@ -0,0 +1,477 @@
+$(function() {
+    $('#a1').hide();
+    $('#a2').hide();
+    $('#a3').hide();
+    $('#a4').hide();
+    $('#a5').hide();
+    $('#a6').hide();
+    $('#a7').hide();
+    $('#a8').hide();
+    $('#a9').hide();
+    $('#a10').hide();
+    $('#a11').hide();
+    $('#a12').hide();
+    $('#a13').hide();
+    $('#a14').hide();
+    $('#a15').hide();
+    $('#a16').hide();
+    $('#a17').hide();
+    $('#a18').hide();
+    $('#a19').hide();
+    $('#a20').hide();
+    $('#a21').hide();
+    $('#a22').hide();
+    $('#a23').hide();
+    $('#a24').hide();
+    $('#a25').hide();
+    $('#a26').hide();
+    $('#a27').hide();
+    $('#a28').hide();
+    $('#a29').hide();
+    $('#a30').hide();
+    $('#a31').hide();
+    $('#a32').hide();
+    $('#a33').hide();
+    $('#a34').hide();
+    $('#a35').hide();
+    $('#a36').hide();
+    $('#a37').hide();
+    $('#a38').hide();
+    $('#a39').hide();
+    $('#a40').hide();
+    $('#a41').hide();
+    $('#a42').hide();
+    $('#a43').hide();
+    $('#a44').hide();
+});
+
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a1') {
+            $('#a1').show();
+        } else {
+            $('#a1').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a2') {
+            $('#a2').show();
+        } else {
+            $('#a2').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a3') {
+            $('#a3').show();
+        } else {
+            $('#a3').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a4') {
+            $('#a4').show();
+        } else {
+            $('#a4').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a5') {
+            $('#a5').show();
+        } else {
+            $('#a5').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a6') {
+            $('#a6').show();
+        } else {
+            $('#a6').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a7') {
+            $('#a7').show();
+        } else {
+            $('#a7').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a8') {
+            $('#a8').show();
+        } else {
+            $('#a8').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a9') {
+            $('#a9').show();
+        } else {
+            $('#a9').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a10') {
+            $('#a10').show();
+        } else {
+            $('#a10').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a11') {
+            $('#a11').show();
+        } else {
+            $('#a11').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a12') {
+            $('#a12').show();
+        } else {
+            $('#a12').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a13') {
+            $('#a13').show();
+        } else {
+            $('#a13').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a14') {
+            $('#a14').show();
+        } else {
+            $('#a14').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a15') {
+            $('#a15').show();
+        } else {
+            $('#a15').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a16') {
+            $('#a16').show();
+        } else {
+            $('#a16').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a17') {
+            $('#a17').show();
+        } else {
+            $('#a17').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a18') {
+            $('#a18').show();
+        } else {
+            $('#a18').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a19') {
+            $('#a19').show();
+        } else {
+            $('#a19').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a20') {
+            $('#a20').show();
+        } else {
+            $('#a20').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a21') {
+            $('#a21').show();
+        } else {
+            $('#a21').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a22') {
+            $('#a22').show();
+        } else {
+            $('#a22').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a23') {
+            $('#a23').show();
+        } else {
+            $('#a23').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a24') {
+            $('#a24').show();
+        } else {
+            $('#a24').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a25') {
+            $('#a25').show();
+        } else {
+            $('#a25').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a26') {
+            $('#a26').show();
+        } else {
+            $('#a26').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a27') {
+            $('#a27').show();
+        } else {
+            $('#a27').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a28') {
+            $('#a28').show();
+        } else {
+            $('#a28').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a29') {
+            $('#a29').show();
+        } else {
+            $('#a29').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a30') {
+            $('#a30').show();
+        } else {
+            $('#a30').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a31') {
+            $('#a31').show();
+        } else {
+            $('#a31').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a32') {
+            $('#a32').show();
+        } else {
+            $('#a32').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a33') {
+            $('#a33').show();
+        } else {
+            $('#a33').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a34') {
+            $('#a34').show();
+        } else {
+            $('#a34').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a35') {
+            $('#a35').show();
+        } else {
+            $('#a35').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a36') {
+            $('#a36').show();
+        } else {
+            $('#a36').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a37') {
+            $('#a37').show();
+        } else {
+            $('#a37').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a38') {
+            $('#a38').show();
+        } else {
+            $('#a38').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a39') {
+            $('#a39').show();
+        } else {
+            $('#a39').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a40') {
+            $('#a40').show();
+        } else {
+            $('#a40').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a41') {
+            $('#a41').show();
+        } else {
+            $('#a41').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a42') {
+            $('#a42').show();
+        } else {
+            $('#a42').hide();
+        }
+    });
+});
+
+$(function() {
+    $('#question').change(function(){
+        if($('#question').val() == 'a43') {
+            $('#a43').show();
+        } else {
+            $('#a43').hide();
+        }
+    });
+});
